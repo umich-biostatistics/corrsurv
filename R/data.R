f840e339bc4ae3ebbe368f1835bfc2140dbfc37c
Kleinsasser <mkleinsa@UMICH.EDU>
2019-10-24 11:50:21 -0400

    example data and doc

---
+++ R/data.R
@@ -0,0 +1,15 @@
+
+#' Paired survival data set for examples and tests.
+#'
+#' Describe the data set.
+#'
+#' @format A \code{data.frame} with 3711 observations (rows) and 6 variables (columns):
+#' \describe{
+#'   \item{trt1}{description, type}
+#'   \item{x1}{description, type}
+#'   \item{delta1}{description, type}
+#'   \item{trt2}{description, type}
+#'   \item{x2}{description, type}
+#'   \item{delta2}{description, type}
+#' }
+"pairdata"
