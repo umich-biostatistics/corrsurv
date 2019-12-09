90e1f79036fb20d75849c58598e5c0f852d0db64
Kleinsasser <mkleinsa@UMICH.EDU>
2019-12-09 14:19:04 -0500

    formatted code

--- R/TM.R
+++ R/TM.R
@@ -196,24 +196,25 @@ RMRL_function = function(data_format, Tau, t) {
     temp1 = t(array(lambda_tk, c(M_time, n)))
     temp2 = t(array(Y_tk/n, c(M_time, n)))
     temp3 = (dN_i_tk - temp1*Y_i_tk)/temp2
-    Z_i_tk_m=t(apply(temp3,1,sum_function))
-    temp4=t(array(S_tk_m,c(M_time,n)))
-    temp5=(temp4*Z_i_tk_m)[,1:(M_time-1)]
-    temp6=t(array(time_int_star,c(M_time-1,n)))*temp5
-    Z_i_tk=cbind(Z_i_tk,apply(temp6,1,sum))
+    Z_i_tk_m = t(apply(temp3, 1, sum_function))
+    temp4 = t(array(S_tk_m, c(M_time, n)))
+    temp5 = (temp4*Z_i_tk_m)[,1:(M_time-1)]
+    temp6 = t(array(time_int_star, c(M_time-1, n)))*temp5
+    Z_i_tk = cbind(Z_i_tk, apply(temp6, 1, sum))
   }
   
-  area_under_RMRL=sum((t[2:b]-t[1:(b-1)])*(RMRL[2:b]+RMRL[1:(b-1)])/2)
+  area_under_RMRL = sum((t[2:b] - t[1:(b-1)])*(RMRL[2:b] + RMRL[1:(b-1)])/2)
   
   #variance of area under RMRL
-  Z_i=rep(0,nrow(Z_i_tk))
-  for(i in 2:b)
-  {
-    Z_i=Z_i + (t[i]-t[i-1])*(Z_i_tk[,i]+Z_i_tk[,i-1])/2
-  }
-  var_area_under_RMRL=var(Z_i)/n
+  Z_i = rep(0, nrow(Z_i_tk))
   
-  list(RMRL=RMRL,area_under_RMRL=area_under_RMRL,var_area_under_RMRL=var_area_under_RMRL)
+  for(i in 2:b) {
+    Z_i = Z_i + (t[i] - t[i-1])*(Z_i_tk[,i] + Z_i_tk[,i-1])/2
+  }
+  var_area_under_RMRL = var(Z_i)/n
+  
+  list(RMRL = RMRL, area_under_RMRL = area_under_RMRL, 
+       var_area_under_RMRL = var_area_under_RMRL)
   # OUTPUT
   # RMRL=restricted mean residual life function evaluated at times defined by t
   # area_under_RMRL=area under RMRL function evaluated at times t
@@ -242,8 +243,8 @@ RMRL_function = function(data_format, Tau, t) {
 #'
 #'
 
-two_sample_recurrent_events_test=function(X,delta,Z,Group,Tau,t,method="average",plot=FALSE)
-{
+two_sample_recurrent_events_test = function(X, delta, Z, Group, Tau, t, 
+                                            method="average", plot=FALSE) {
   ############format observed data for our method
   # INPUT
   # X=vector containing observed time to terminating event
@@ -252,54 +253,61 @@ two_sample_recurrent_events_test=function(X,delta,Z,Group,Tau,t,method="average"
   # Group=vector indicating which group each patient belongs to
   # t=vector containing start times of follow-up windows chosen
   
-  Group_id=unique(Group)
-  formatted_group1_data=format_data_ourmethod(X=X[Group==Group_id[1]],delta=delta[Group==Group_id[1]],Z=Z[Group==Group_id[1],],t)
-  formatted_group2_data=format_data_ourmethod(X=X[Group==Group_id[2]],delta=delta[Group==Group_id[2]],Z=Z[Group==Group_id[2],],t)  
+  Group_id = unique(Group)
+  formatted_group1_data = format_data_ourmethod(X = X[Group == Group_id[1]],
+                                                delta = delta[Group == Group_id[1]],
+                                                Z = Z[Group == Group_id[1],], t)
+  formatted_group2_data = format_data_ourmethod(X = X[Group == Group_id[2]],
+                                                delta = delta[Group == Group_id[2]],
+                                                Z = Z[Group == Group_id[2],], t)  
   
-  if(method=="average")
-  {
-    results1=get_mu_hat_star_tau(formatted_group1_data$X_tk,formatted_group1_data$delta_tk,Tau=Tau,t=t)
-    results2=get_mu_hat_star_tau(formatted_group2_data$X_tk,formatted_group2_data$delta_tk,Tau=Tau,t=t)
-    difference=results1$mean-results2$mean
-    sd_diff=sqrt(results1$var+results2$var)
-    test_stat=abs(difference/sd_diff)
-    test_stat_p=2*(1-pnorm(test_stat))
-    mean12=c(results1$mean,results2$mean)
-    var12=c(results1$var,results2$var)
+  if(method == "average") {
+    results1 = get_mu_hat_star_tau(formatted_group1_data$X_tk, 
+                                   formatted_group1_data$delta_tk, Tau = Tau, t = t)
+    results2 = get_mu_hat_star_tau(formatted_group2_data$X_tk, 
+                                   formatted_group2_data$delta_tk, Tau = Tau, t = t)
+    difference = results1$mean - results2$mean
+    sd_diff = sqrt(results1$var + results2$var)
+    test_stat = abs(difference / sd_diff)
+    test_stat_p = 2*(1 - pnorm(test_stat))
+    mean12 = c(results1$mean, results2$mean)
+    var12 = c(results1$var, results2$var)
   }
   
-  if(method=="area")
-  {
-    results1=RMRL_function(data_format=formatted_group1_data,Tau=Tau,t=t)
-    results2=RMRL_function(data_format=formatted_group2_data,Tau=Tau,t=t)
-    difference=results1$area_under_RMRL-results2$area_under_RMRL
-    sd_diff=sqrt(results1$var_area_under_RMRL+results2$var_area_under_RMRL)
-    test_stat=abs(difference/sd_diff)
-    test_stat_p=2*(1-pnorm(test_stat))
-    mean12=c(results1$area_under_RMRL,results2$area_under_RMRL)
-    var12=c(results1$var_area_under_RMRL,results2$var_area_under_RMRL)   
+  if(method == "area") {
+    results1 = RMRL_function(data_format = formatted_group1_data, Tau = Tau, t = t)
+    results2 = RMRL_function(data_format = formatted_group2_data, Tau = Tau, t = t)
+    difference = results1$area_under_RMRL - results2$area_under_RMRL
+    sd_diff = sqrt(results1$var_area_under_RMRL + results2$var_area_under_RMRL)
+    test_stat = abs(difference / sd_diff)
+    test_stat_p = 2*(1 - pnorm(test_stat))
+    mean12 = c(results1$area_under_RMRL, results2$area_under_RMRL)
+    var12 = c(results1$var_area_under_RMRL, results2$var_area_under_RMRL)   
     
-    if(plot)
-    {
-      RMRL1=results1$RMRL
-      RMRL2=results2$RMRL
-      xadj=Tau/50
-      yadj1=max(RMRL1,RMRL2)/20
-      yadj2=max(RMRL1,RMRL2)/50
-      matplot(cbind(t,t),cbind(RMRL1,RMRL2),type="l",xaxs="i",xaxt="n",yaxs="i",xlab="Time",ylab="RMRL",xlim=c(t[1]-xadj,t[length(t)]+xadj),ylim=c(min(RMRL1,RMRL2)-yadj1,max(RMRL1,RMRL2)+yadj2))
-      points(t,RMRL1,pch=20,cex=1,col="black")
-      points(t,RMRL2,pch=20,cex=1,col="red")
-      axis(1,at=t,labels=t)
-      legend("bottomright",c("Group 1","Group 2"),col=c("black","red"),lty=c(1,2),bty="n")
+    if(plot) {
+      RMRL1 = results1$RMRL
+      RMRL2 = results2$RMRL
+      xadj = Tau/50
+      yadj1 = max(RMRL1, RMRL2)/20
+      yadj2 = max(RMRL1, RMRL2)/50
+      matplot(cbind(t, t), cbind(RMRL1, RMRL2), type = "l", xaxs = "i", xaxt = "n", 
+              yaxs = "i", xlab = "Time", ylab= "RMRL", xlim = c(t[1] - xadj, t[length(t)] + xadj), 
+              ylim = c(min(RMRL1, RMRL2) - yadj1, max(RMRL1, RMRL2) + yadj2))
+      points(t, RMRL1, pch = 20, cex = 1, col = "black")
+      points(t, RMRL2, pch = 20, cex = 1, col = "red")
+      axis(1, at = t, labels = t)
+      legend("bottomright", c("Group 1", "Group 2"), col = c("black", "red"), 
+             lty = c(1, 2), bty = "n")
     }
   }
   
-  test_stat_p_print=paste("=",round(test_stat_p,5))
-  if(round(test_stat_p,5)==0){test_stat_p_print="<0.00001"}
-  follow_up_windows_print=NULL
-  for(k in 1:length(t))
-  {
-    follow_up_windows_print=paste(follow_up_windows_print,paste("[",t[k],",",t[k]+Tau,"]"),"     ")
+  test_stat_p_print = paste("=", round(test_stat_p, 5))
+  if(round(test_stat_p, 5) == 0) { test_stat_p_print = "<0.00001" }
+  follow_up_windows_print = NULL
+  
+  for(k in 1:length(t)) {
+    follow_up_windows_print = paste(follow_up_windows_print, 
+                                    paste("[", t[k], ",", t[k] + Tau, "]"), "     ")
   }
   
   #PRINT OUTPUT
@@ -327,8 +335,8 @@ two_sample_recurrent_events_test=function(X,delta,Z,Group,Tau,t,method="average"
   cat(follow_up_windows_print)
   cat("\n")
   cat("\n")
-  if(method=="average")
-  {
+  
+  if(method == "average") {
     cat("Sample Estimates: Average restricted mean survival across follow-up windows")
     cat("\n")
     cat(paste("Group 1: ",round(results1$mean,5)))
@@ -339,21 +347,28 @@ two_sample_recurrent_events_test=function(X,delta,Z,Group,Tau,t,method="average"
     cat("Alternative hypothesis: True difference in the average restricted mean survival across follow-up windows is not equal to 0")
     cat("\n")
   }
-  if(method=="area")
-  {
+  
+  if(method == "area") {
     cat("Sample Estimates: Area under RMRL function evaluated for each follow-up window")
     cat("\n")
-    cat(paste("Group 1: ",round(results1$area_under_RMRL,5)))
+    cat(paste("Group 1: ", round(results1$area_under_RMRL, 5)))
     cat("\n")
-    cat(paste("Group 2: ",round(results2$area_under_RMRL,5)))
+    cat(paste("Group 2: ", round(results2$area_under_RMRL, 5)))
     cat("\n")
     cat("\n")
     cat("Alternative hypothesis: True difference in the area under the RMRL functions is not equal to 0")
     cat("\n")
   }
-  cat(paste("Test-statistic=",round(test_stat,5), ", p-value", test_stat_p_print))
+  cat(paste("Test-statistic=", round(test_stat, 5), ", p-value", test_stat_p_print))
   
-  return(list(Mean=mean12,Var=var12,test_stat=test_stat,test_stat_p=test_stat_p))
+  return(
+    list(
+      Mean = mean12, 
+      Var = var12, 
+      test_stat = test_stat, 
+      test_stat_p = test_stat_p
+      )
+    )
   # OUTPUT
   # Mean= vector containing sample estimates of overall tau-restricted mean survival in each group
   # Var= vector containing empirical variance of estimates of overall tau-restricted mean survival in each group
@@ -380,35 +395,40 @@ format_data_AG=function(X, delta, Z_star)
 {
   ############format observed data for Andersen-Gill method
   #load data
-  n=length(X)
+  n = length(X)
   
-  X_Z_star=cbind(Z_star,X)
-  T_start=NULL
-  T_stop=NULL
-  status=NULL
-  ID_AG=NULL
-  for(i in 1:n)
-  {
-    T_stop_subject=as.numeric(na.omit(X_Z_star[i,]))
-    n_reccurent_subject=length(T_stop_subject)-1
-    if(n_reccurent_subject>0)
-    {
-      T_start_subject=c(0,T_stop_subject[1:n_reccurent_subject])
+  X_Z_star = cbind(Z_star, X)
+  T_start = NULL
+  T_stop = NULL
+  status = NULL
+  ID_AG = NULL
+  for(i in 1:n) {
+    T_stop_subject = as.numeric(na.omit(X_Z_star[i,]))
+    n_reccurent_subject = length(T_stop_subject) - 1
+    
+    if(n_reccurent_subject > 0) {
+      T_start_subject = c(0, T_stop_subject[1:n_reccurent_subject])
     }
-    if(n_reccurent_subject==0)
-    {
-      T_start_subject=0
+    
+    if(n_reccurent_subject == 0) {
+      T_start_subject = 0
     }    
-    status_subject=c(rep(1,n_reccurent_subject),delta[i])
+    status_subject = c(rep(1, n_reccurent_subject), delta[i])
     
-    T_start=c(T_start,T_start_subject)
-    T_stop=c(T_stop,T_stop_subject)
-    status=c(status,status_subject)  
-    ID_AG=c(ID_AG,rep(i,n_reccurent_subject+1))
+    T_start = c(T_start, T_start_subject)
+    T_stop = c(T_stop, T_stop_subject)
+    status = c(status, status_subject)  
+    ID_AG = c(ID_AG, rep(i, n_reccurent_subject + 1))
     
-    if(length(T_start)!=length(T_stop)){print(i)}
+    if(length(T_start) != length(T_stop)) { print(i) }
   }
-  list(ID=ID_AG,T_start=T_start,T_stop=T_stop,status=status)
+  
+  list(
+    ID = ID_AG,
+    T_start = T_start, 
+    T_stop = T_stop, 
+    status = status
+    )
 }
 
 #' Andersen-Gill test
@@ -422,19 +442,21 @@ format_data_AG=function(X, delta, Z_star)
 #' }
 #'
 
-AG_test_stat=function(data1_format,data2_format)
-{
+AG_test_stat = function(data1_format, data2_format) {
   #Andersen-Gill model based two-sample test
-  n1=length(data1_format$ID)
-  treatment=c(rep(0,length(data1_format$ID)),rep(1,length(data2_format$ID)))
-  T_start=c(data1_format$T_start,data2_format$T_start)
-  T_stop=c(data1_format$T_stop,data2_format$T_stop)
-  status=c(data1_format$status,data2_format$status)
-  id=c(data1_format$ID,data2_format$ID+n1)
-  ag_model=coxph(Surv(T_start, T_stop, status) ~ treatment + cluster(id))
-  test_stat_p=1-pchisq(ag_model$wald.test,1)
-  list(test_stat_p=test_stat_p)
+  n1 = length(data1_format$ID)
+  treatment = c(rep(0, length(data1_format$ID)), rep(1, length(data2_format$ID)))
+  T_start = c(data1_format$T_start, data2_format$T_start)
+  T_stop = c(data1_format$T_stop, data2_format$T_stop)
+  status = c(data1_format$status, data2_format$status)
+  id = c(data1_format$ID, data2_format$ID + n1)
+  ag_model = coxph(Surv(T_start, T_stop, status) ~ treatment + cluster(id))
+  test_stat_p = 1 - pchisq(ag_model$wald.test, 1)
+  list(
+    test_stat_p = test_stat_p
+    )
 }
+
 
 #' Format observed data for Ghosh and Lin method
 #'
@@ -455,60 +477,65 @@ AG_test_stat=function(data1_format,data2_format)
 #' }
 #'
 
-format_data_GL=function(X, delta, Z_star,time)
-{
+format_data_GL = function(X, delta, Z_star, time) {
   ############format observed data for Ghosh and Lin method
   #load data
-  n_time=length(time)
-  n=length(X)
+  n_time = length(time)
+  n = length(X)
   
-  time_array=t(array(time,c(n_time,n)))
-  delta_array=array(delta,c(n,n_time))
-  dN_D_i=array(as.numeric(X==time_array & delta_array==1),c(n,n_time))
-  dN_D=apply(dN_D_i,2,sum)
-  Y_i=array(as.numeric(X>=time_array),c(n,n_time))
-  Y=apply(Y_i,2,sum)
-  time_array2<<-t(array(time,c(n_time,ncol(Z_star))))
-  dN_i=t(apply(Z_star,1,get_dN_for_i))
+  time_array = t(array(time, c(n_time, n)))
+  delta_array = array(delta, c(n, n_time))
+  dN_D_i = array(as.numeric(X == time_array & delta_array == 1), c(n, n_time))
+  dN_D = apply(dN_D_i, 2, sum)
+  Y_i = array(as.numeric(X >= time_array), c(n, n_time))
+  Y = apply(Y_i, 2, sum)
+  time_array2 <<- t(array(time, c(n_time, ncol(Z_star))))
+  dN_i = t(apply(Z_star, 1, get_dN_for_i))
   
-  dN=apply(dN_i,2,sum)
-  dR_hat=dN/Y
-  dch_hat=dN_D/Y
-  dM_i=dN_i - Y_i*t(array(dR_hat,c(n_time,n)))
-  dM_D_i=dN_D_i - Y_i*t(array(dch_hat,c(n_time,n)))
+  dN = apply(dN_i, 2, sum)
+  dR_hat = dN/Y
+  dch_hat = dN_D/Y
+  dM_i = dN_i - Y_i*t(array(dR_hat, c(n_time, n)))
+  dM_D_i = dN_D_i - Y_i*t(array(dch_hat, c(n_time, n)))
   
   #calculate ch estimate
-  temp=t(array(dch_hat,c(n_time,n_time)))
-  upperTriangle(temp,diag=F)=0
-  ch_hat=apply(temp,1,sum)
+  temp = t(array(dch_hat, c(n_time, n_time)))
+  upperTriangle(temp, diag = F) = 0
+  ch_hat = apply(temp, 1, sum)
   
   #calculate survival estimate
-  s_hat=exp(-ch_hat)
+  s_hat = exp(-ch_hat)
   
   #calculate mu estimate
-  dmu_hat=s_hat*dR_hat
-  temp=t(array(dmu_hat,c(n_time,n_time)))
-  upperTriangle(temp,diag=F)=0
-  mu_hat=apply(temp,1,sum)  
+  dmu_hat = s_hat*dR_hat
+  temp = t(array(dmu_hat, c(n_time, n_time)))
+  upperTriangle(temp, diag = F) = 0
+  mu_hat = apply(temp, 1, sum)  
   
   #calculate psi estimate
-  temp=dM_D_i*t(array(n/Y,c(n_time,n)))
-  temp2=t(apply(temp,1,sum_function))
-  dpsi_i=dM_i*t(array(s_hat*n/Y,c(n_time,n)))-temp2*t(array(dmu_hat,c(n_time,n)))
+  temp = dM_D_i*t(array(n/Y, c(n_time, n)))
+  temp2 = t(apply(temp, 1, sum_function))
+  dpsi_i = dM_i*t(array(s_hat*n/Y, c(n_time, n))) - temp2*t(array(dmu_hat, c(n_time, n)))
   
   #calculate last observed event time
-  tau=max(X*delta)
+  tau = max(X*delta)
   
-  list(Y=Y,tau=tau,dmu_hat=dmu_hat,dch_hat=dch_hat,dpsi_i=dpsi_i,dM_D_i=dM_D_i)
+  list(
+    Y = Y,
+    tau = tau,
+    dmu_hat = dmu_hat,
+    dch_hat = dch_hat,
+    dpsi_i = dpsi_i,
+    dM_D_i = dM_D_i
+    )
 }
 
 #' Helper to get deaths at time i
 #'
 
-get_dN_for_i=function(X1)
-{
-  temp=(X1==time_array2)
-  apply(temp,2,sum,na.rm=T)
+get_dN_for_i = function(X1) {
+  temp = (X1 == time_array2)
+  apply(temp, 2, sum, na.rm = T)
 }
 
 #' Calculate test statistic for Ghosh and Lin method
@@ -523,48 +550,52 @@ get_dN_for_i=function(X1)
 #'   \item{test_stat_p}{P-value for test statistic}
 #' }
 
-GL_test_stat=function(p_GL,time,data1_format,data2_format)
-{
+GL_test_stat = function(p_GL, time, data1_format, data2_format) {
   #Load data
-  Y1=data1_format$Y
-  Y2=data2_format$Y
-  tau1=data1_format$tau
-  tau2=data2_format$tau
-  dmu_hat1=data1_format$dmu_hat
-  dmu_hat2=data2_format$dmu_hat
-  dch_hat1=data1_format$dch_hat
-  dch_hat2=data2_format$dch_hat
-  dpsi_i1=data1_format$dpsi_i
-  dpsi_i2=data2_format$dpsi_i
-  dM_D_i1=data1_format$dM_D_i
-  dM_D_i2=data2_format$dM_D_i
+  Y1 = data1_format$Y
+  Y2 = data2_format$Y
+  tau1 = data1_format$tau
+  tau2 = data2_format$tau
+  dmu_hat1 = data1_format$dmu_hat
+  dmu_hat2 = data2_format$dmu_hat
+  dch_hat1 = data1_format$dch_hat
+  dch_hat2 = data2_format$dch_hat
+  dpsi_i1 = data1_format$dpsi_i
+  dpsi_i2 = data2_format$dpsi_i
+  dM_D_i1 = data1_format$dM_D_i
+  dM_D_i2 = data2_format$dM_D_i
   
-  n_time=length(time)
+  n_time = length(time)
   
-  n1=nrow(dpsi_i1)
-  n2=nrow(dpsi_i2)
+  n1 = nrow(dpsi_i1)
+  n2 = nrow(dpsi_i2)
   
-  tau=min(tau1,tau2)
-  time_used=rep(1,length(time))
-  time_used[time>tau]=0
+  tau = min(tau1, tau2)
+  time_used = rep(1, length(time))
+  time_used[time > tau] = 0
   
   #calculate test stats
-  K_LR=Y1*Y2*(n1+n2)/((Y1+Y2)*n1*n2)
-  Q_LR=sum((K_LR*(dmu_hat1-dmu_hat2))*time_used)
-  Q_D=sum((K_LR*(dch_hat1-dch_hat2))*time_used)
-  Q_WCT=p_GL*Q_LR + (1-p_GL)*Q_D
+  K_LR = Y1*Y2*(n1 + n2) / ((Y1 + Y2)*n1*n2)
+  Q_LR = sum((K_LR*(dmu_hat1 - dmu_hat2))*time_used)
+  Q_D = sum((K_LR*(dch_hat1 - dch_hat2))*time_used)
+  Q_WCT = p_GL*Q_LR + (1 - p_GL)*Q_D
   
   #calculate variance of Q_WCT
-  rho_hat1=n1/(n1+n2)
-  rho_hat2=n2/(n1+n2)
-  U_i1=apply((p_GL*dpsi_i1 + (1-p_GL)*dM_D_i1*t(array(n1/Y1,c(n_time,n1))))*t(array(K_LR,c(n_time,n1))),1,sum)
-  U_i2=apply((p_GL*dpsi_i2 + (1-p_GL)*dM_D_i2*t(array(n2/Y2,c(n_time,n2))))*t(array(K_LR,c(n_time,n2))),1,sum)
-  var_Q_WCT=rho_hat2*mean(U_i1^2) + rho_hat1*mean(U_i2^2)
+  rho_hat1 = n1 / (n1 + n2)
+  rho_hat2 = n2 / (n1 + n2)
+  U_i1 = apply((p_GL*dpsi_i1 + (1 - p_GL)*dM_D_i1*t(array(n1/Y1, c(n_time, n1))))*
+                 t(array(K_LR, c(n_time, n1))), 1, sum)
+  U_i2 = apply((p_GL*dpsi_i2 + (1 - p_GL)*dM_D_i2*t(array(n2/Y2, c(n_time, n2))))*
+                 t(array(K_LR, c(n_time, n2))), 1, sum)
+  var_Q_WCT = rho_hat2*mean(U_i1^2) + rho_hat1*mean(U_i2^2)
   
   #calculate test statistic that has standard normal distribution under H0
-  test_stat=sqrt(n1*n2/(n1+n2))*Q_WCT/sqrt(var_Q_WCT)
-  test_stat_p=2*(1-pnorm(abs(test_stat)))
-  list(test_stat_p=test_stat_p)
+  test_stat = sqrt(n1*n2 / (n1+n2))*Q_WCT / sqrt(var_Q_WCT)
+  test_stat_p = 2*(1 - pnorm(abs(test_stat)))
+  
+  list(
+    test_stat_p = test_stat_p
+    )
 }
 
 
